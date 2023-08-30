8db12ba950e17ab05c2f0b09b5030cf6021d4af0
banana-king778 <gamingthingemail@gmail.com>
2023-08-29 21:24:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require('fs').promises;
 
 const host = "0.0.0.0";
-const port = 80;
+const port = 8084;
 
 const requestListener = function (req, res) {
     fs.readFile(__dirname + "/index.html")
